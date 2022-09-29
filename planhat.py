64000f2fc1df7a395c88d503846147f25a89375a
v-grun <viktor.grunwald@gmail.com>
2022-09-29 14:53:58 +0200

    updated the code

--- planhat.py
+++ planhat.py
@@ -1,6 +1,8 @@
 from planhat.client import Planhat
 import settings
-import pprint
+import pandas as pd
+import pandas_gbq
+from google.oauth2.service_account import Credentials
 
 from planhat import API_URL, API_URL_EU, API_URL_US2
 
@@ -11,5 +13,23 @@ planhat_client = Planhat(
 
 companies_data = planhat_client.get_companies()
 licenses_data = planhat_client.get_licenses()
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(companies_data)
+
+# df = pd.json_normalize(licenses_data)
+df = pd.DataFrame(licenses_data)
+
+credentials = Credentials.from_service_account_file(
+    "/Users/viktorgrunwald/Documents/Programming/planhat-python/service_account_bq_licenses.json"
+)
+target_table = "dbtcourse-356512.planhat_data.test_licenses"
+project_id = "dbtcourse-356512"
+job_location = "EU"
+
+pandas_gbq.to_gbq(
+    df,
+    target_table,
+    project_id=project_id,
+    credentials=credentials,
+    if_exists="replace",
+)
+
+print(df)
